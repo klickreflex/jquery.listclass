559d19cefbe8707269554d7b15ae0cdf51817fff
Daniel Wentsch <hello@wentsch.me>
2016-07-05 09:29:51 +0200

    New option apply_on_parents to prevent the script being applied to lists that contain lists
    Allows

--- jquery.listclass.js
+++ jquery.listclass.js
@@ -4,14 +4,22 @@
     var settings = $.extend({
         // These are the defaults.
         item_limit:       "4",
-        class_name:       "multi-column"
+        class_name:       "multi-column",
+        apply_on_parents: false
     }, options );
 
     return this.each(function() {
       var $this = $(this);
       list_size = $this.find("li").length;
-      if(list_size >= settings.item_limit) {
-        $this.addClass(settings.class_name);
+      // if there's another <ul> inside our selector:
+      if($this.has('ul')) {
+        has_children = true;
+      }
+
+      if(!has_children || settings.apply_on_parents) {
+        if(list_size >= settings.item_limit) {
+          $this.addClass(settings.class_name);
+        }
       }
     });
   };
